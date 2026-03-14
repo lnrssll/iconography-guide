062292acd1b2f85a2cd80e5bb41833176328072d
lane <lnrssll@pm.me>
2026-03-14 12:59:03 -0700

    vite config update for docker deploy

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,15 @@ export default defineConfig(({ mode }) => {
       }),
       build({ entry: './src/index.tsx' }),
     ],
+    build: {
+      rollupOptions: {
+        output: {
+          banner: `import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);`,
+        },
+      },
+    },
   }
 })
